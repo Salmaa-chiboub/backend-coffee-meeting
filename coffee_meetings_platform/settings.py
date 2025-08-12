26c148c2a994e775a50f01f01ba99cb690edabe1
Salmaa-chiboub <salmachiboub08@gmail.com>
2025-08-12 14:19:32 +0100

    fixing CORS

--- coffee_meetings_platform/settings.py
+++ coffee_meetings_platform/settings.py
@@ -311,6 +311,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3001",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "https://web-production-828b8.up.railway.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
