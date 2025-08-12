ebc90d72158712c36b9953bf9bcb35059b2a4dfd
Salmaa-chiboub <salmachiboub08@gmail.com>
2025-08-12 18:38:45 +0100

    fix

--- coffee_meetings_platform/settings.py
+++ coffee_meetings_platform/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
     # CSRF middleware disabled for API endpoints - JWT authentication is used instead
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
